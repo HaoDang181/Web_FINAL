a31f7812e88942b6c5dbc8765798c0bc85cb792f
bangdiep <dtkhanhbang.04204@gmail.com>
2024-05-03 20:17:04 +0700

    update

--- src/user/update-lock-status.php
+++ src/user/update-lock-status.php
@@ -2,18 +2,18 @@
 
 require_once '../db-connect.php';
 
-if(isset($_POST['username'], $_POST['lock_status'])){
-    $username = $_POST['username'];
+if(isset($_POST['id'], $_POST['lock_status'])){
+    $id = $_POST['id'];
     $lock_status = $_POST['lock_status'];
 
     echo $lock_status;
     // Prepare the SQL statement to select all data from the table
-    $sql = "Update user_account SET is_lock = :lock_status WHERE username = :username";
+    $sql = "Update user_account SET is_lock = :lock_status WHERE id = :id";
 
     // Prepare and execute the SQL statement with PDO
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':lock_status', $lock_status);
-    $stmt->bindParam(':username', $username);
+    $stmt->bindParam(':id', $id);
 
     // Execute the statement
     if ($stmt->execute()) {
